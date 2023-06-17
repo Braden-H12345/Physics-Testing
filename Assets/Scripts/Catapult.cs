60b818e335833954c3070412a102e2a74bc3518d
Braden-H12345 <68672794+Braden-H12345@users.noreply.github.com>
2023-06-17 18:42:49 -0500

    Update Catapult.cs
    Should allow the UI menu to open. I am not on a machine with unity though so cannot test! There is also no UI to open yet haha

--- Assets/Scripts/Catapult.cs
+++ Assets/Scripts/Catapult.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[SerializeField] GameObject DisplayPanelForRigidbody
+[SerializeField] Text TextForNoRigidbody
 public class Catapult : MonoBehaviour
 {
 
@@ -9,12 +12,35 @@ public class Catapult : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TextForNoRigidbody.setActive(False);
+        DisplayPanelForRigidbody.setActive(False);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.GetComponent<Rigidbody>())
+        {
+            DisplayPanelForRigidbody.setActive(True);
+        }
+        else
+        {
+            TextForNoRigidbody.text = "Object to be launched must have a Rigidbody component!"
+            Debug.Log("Error: game object - " + TextForNoRigidbody.GameObject.name + " has no Rigidbody component!"
+            TextForNoRigidbody.setActive(True);
+        }
+        
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        TextForNoRigidbody.setActive(False);
+        DisplayPanelForRigidbody.setActive(False);
+
     }
 }
